bf27d90e059aa8cd667f3330f0b572b14fbe8902
just-ed <ed_tomeyan@mail.ru>
2019-04-14 01:25:15 +0300

    log in user after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, notice: 'Welcome! :)'
     else
       render :new
