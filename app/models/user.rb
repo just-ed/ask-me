21977f6241a5176a9d8b392ea3f7301b6deffbce
just-ed <ed_tomeyan@mail.ru>
2019-04-14 14:03:16 +0300

    delete questions of deleted user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
 
   attr_accessor :password
 
-  has_many :questions
-  has_many :authored_questions, class_name: 'Question', foreign_key: 'author_id'
+  has_many :questions, dependent: :destroy
+  has_many :authored_questions, class_name: 'Question', foreign_key: 'author_id', dependent: :nullify
 
   validates :username,
             presence: true,
