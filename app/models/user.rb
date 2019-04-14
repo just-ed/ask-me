9fbe3c99f5d00ee33bc64c003b90232df2dea00b
just-ed <ed_tomeyan@mail.ru>
2019-04-14 14:05:51 +0300

    email validation case_sensitive false

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
 
   validates :email,
             presence: true,
-            uniqueness: true,
+            uniqueness: { case_sensitive: false },
             format: { with: URI::MailTo::EMAIL_REGEXP }
 
   validates :password,
