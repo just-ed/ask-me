90ec128a68a760f5fb2d092fa584e8131094e14a
just-ed <ed_tomeyan@mail.ru>
2019-04-19 11:44:06 +0300

    bug fix hashtag_links

--- app/helpers/hashtags_helper.rb
+++ app/helpers/hashtags_helper.rb
@@ -1,5 +1,5 @@
 module HashtagsHelper
   def add_hashtag_links(s)
-    s.gsub(Hashtag::REGEXP) { |name| link_to name, hashtag_url(name) }.html_safe
+    s.gsub(Hashtag::REGEXP) { |name| link_to name, hashtag_url(name.downcase) }.html_safe
   end
 end
